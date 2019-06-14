50ae7eab848f4ab83b208677c94f3c525f07e98a
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-06-14 09:17:26 +0530

    added specialties, added to vet. Closes #28.

--- pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/models/BaseEntity.java
@@ -2,7 +2,7 @@ package com.vinaylogics.springbootpetclinic.models;
 
 import java.io.Serializable;
 
-public class BaseEntity  implements Serializable {
+public abstract class BaseEntity  implements Serializable {
     private static final long serialVersionUID = -6564417582155434638L;
 
     private Long id;
