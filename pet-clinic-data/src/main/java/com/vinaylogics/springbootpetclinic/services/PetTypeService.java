50bcc958ffb48390f92cd18346413f93984cae59
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-06-14 09:31:59 +0530

    Added petype service. closes #30.

---
+++ pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.vinaylogics.springbootpetclinic.services;
+
+import com.vinaylogics.springbootpetclinic.models.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
