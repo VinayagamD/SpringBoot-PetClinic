87cbdaa9bf1aa2957ea804d8c150db590c7b6dcc
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-06-16 23:31:07 +0530

    added spring data jpa service for specialty. Closes #47.

---
+++ pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,48 @@
+package com.vinaylogics.springbootpetclinic.services.springdatajpa;
+
+import com.vinaylogics.springbootpetclinic.models.Specialty;
+import com.vinaylogics.springbootpetclinic.repositories.SpecialtyRepository;
+import com.vinaylogics.springbootpetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService  implements SpecialityService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().iterator().forEachRemaining(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
