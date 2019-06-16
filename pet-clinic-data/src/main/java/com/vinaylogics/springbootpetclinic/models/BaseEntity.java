68c5cea518875dcd27e2903273603d9dbef362fa
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-06-16 20:04:36 +0530

    Updated Based Entity with JPA annotation. Closes #36.

--- pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/models/BaseEntity.java
@@ -1,10 +1,17 @@
 package com.vinaylogics.springbootpetclinic.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public abstract class BaseEntity  implements Serializable {
     private static final long serialVersionUID = -6564417582155434638L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
