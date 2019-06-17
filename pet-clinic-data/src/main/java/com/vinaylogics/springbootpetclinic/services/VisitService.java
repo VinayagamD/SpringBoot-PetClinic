6313477851d7887b38c48eb29b8d82927bf4e472
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-06-18 00:08:33 +0530

    reqord Implemented Map impl for Visit Service. Closes #48.

---
+++ pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.vinaylogics.springbootpetclinic.services;
+
+import com.vinaylogics.springbootpetclinic.models.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package com.vinaylogics.springbootpetclinic.services.map;
+
+import com.vinaylogics.springbootpetclinic.models.Visit;
+import com.vinaylogics.springbootpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null)
+            throw  new RuntimeException("Invalid Visit");
+        return super.save(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+}
