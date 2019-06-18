1726c2077bf584dc17ecda44acd1eaf6dc004993
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-06-19 00:07:29 +0530

    Added Project Lombok to project. Closes #53.

--- pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/vinaylogics/springbootpetclinic/models/BaseEntity.java
@@ -1,11 +1,20 @@
 package com.vinaylogics.springbootpetclinic.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public abstract class BaseEntity  implements Serializable {
     private static final long serialVersionUID = -6564417582155434638L;
@@ -14,11 +23,4 @@ public abstract class BaseEntity  implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
